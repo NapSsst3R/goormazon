236d64277c26c35e36c9c429427ff0332bd0e3dd
NapSsst3R <napssst3r@gmail.com>
2014-07-03 19:39:37 +0500

    main page - slider change, img product centered

--- local/templates/main/components/bitrix/news.list/slideshow/template.php
+++ local/templates/main/components/bitrix/news.list/slideshow/template.php
@@ -1,25 +1,26 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<div class="links-slider col-xs-12" style="text-align:center;">
-    <ul class="list-unstyled clearfix slideshow_main">
-        <?foreach($arResult["ITEMS"] as $k => $arItem):?>
-        <li
-            class="col-xs-<?=(intval(12/count($arResult["ITEMS"]))<1)?1:intval(12/count($arResult["ITEMS"]));?>"><a
-                href="#<?=$k+1?>"
-                data-liquidslider-ref="main-slider"
-                ><p><?=$arItem['NAME']?></p></a></li>
-        <?endforeach;?>
-    </ul>
-</div>
-<div id="main-slider" class="liquid-slider">
-    <?foreach($arResult["ITEMS"] as $arItem):?>
-    <?
-        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-    ?>
-    <div id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-        <h1 class="title"><?=$arItem['NAME']?></h1>
-        <a href="<?=$arItem['PROPERTIES']['HREF']['VALUE']?>"><img src="<?=$arItem['DETAIL_PICTURE']['SRC']?>" class="img-responsive centred_img" alt="<?=$arItem['NAME']?>"></a>
+<div class="clearfix wrap-slider" style="overflow: hidden;">
+    <div id="main-slider" class="liquid-slider">
+        <?foreach($arResult["ITEMS"] as $arItem):?>
+        <?
+            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+        ?>
+        <div id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+            <h1 class="title"><?=$arItem['NAME']?></h1>
+            <a href="<?=$arItem['PROPERTIES']['HREF']['VALUE']?>"><img src="<?=$arItem['DETAIL_PICTURE']['SRC']?>" class="img-responsive centred_img" alt="<?=$arItem['NAME']?>"></a>
+        </div>
+        <?endforeach?>
     </div>
-    <?endforeach?>
+    <div class="links-slider" style="margin-left:-<?=count($arResult["ITEMS"])*18/2?>px">
+        <ul class="floatlist clearfix list-unstyled slideshow_main">
+            <?foreach($arResult["ITEMS"] as $k => $arItem):?>
+                <li><a
+                        href="#<?=$k+1?>"
+                        data-liquidslider-ref="main-slider"
+                        class="bullets_liquid"
+                        ></a></li>
+            <?endforeach;?>
+        </ul>
+    </div>
 </div>
-
